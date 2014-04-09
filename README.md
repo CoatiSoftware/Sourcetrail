4b4c9f56d89e6772f5d37b21fd85cb178546a254
Eberhard Graether <egraether@gmail.com>
2014-04-09 16:25:32 +0200

    setup: Added clang and llvm
    Adds clang and llvm include directories and libraries to the build. The
    environment variable CLANG_DIR is used to define the location of llvm
    and clang. A simple clang tool implementation was added to assure proper
    operation of the libraries.

--- README.md
+++ README.md
@@ -7,11 +7,13 @@
 * QT 5.2.1
 * CxxTest 4.3
 * Valgrind 3.9.0(linux, macOS)
+* Clang & LLVM (installation guide http://clang.llvm.org/docs/LibASTMatchersTutorial.html)
 
 ##### Environment Variables
 
 * QT_DIR - ...\Qt\Qt5.2.1\5.2.1\<IDE>\
 * CXX_TEST_DIR - .../cxxtest-4.3/
+* CLANG_DIR - .../clang-llvm/
 
 For Win32:
 * VLD_DIR - .../Visual Leak Detector
---
+++ cmake/CLANGConfig.cmake
@@ -0,0 +1,50 @@
+execute_process(
+	COMMAND $ENV{CLANG_DIR}/build/bin/llvm-config --cxxflags
+	OUTPUT_VARIABLE CLANG_DEFINITIONS
+)
+
+set(CLANG_DEFINITIONS ${CLANG_DEFINITIONS} "-fno-rtti")
+
+set(CLANG_INCLUDE_DIRS
+	"$ENV{CLANG_DIR}/llvm/tools/clang/include"
+	"$ENV{CLANG_DIR}/build/tools/clang/include"
+)
+
+set(CLANG_LIBRARY_DIRS "$ENV{CLANG_DIR}/build/lib")
+
+set(CLANG_LIBRARIES
+	#clangApplyReplacements
+	#clangARCMigrate
+	#clangCodeGen
+	#clangDynamicASTMatchers
+	#clangFormat
+	#clangFrontendTool
+	#clangIndex
+	#clangQuer
+	#clangTidy
+	#clangTidyGoogleModule
+	#clangTidyLLVMModule
+	#clangTidyMiscModule
+	clangASTMatchers
+	clangFrontend
+	clangSerialization
+	clangDriver
+	clangTooling
+	clangParse
+	clangSema
+	clangStaticAnalyzerFrontend
+	clangStaticAnalyzerCheckers
+	clangStaticAnalyzerCore
+	clangAnalysis
+	clangRewriteFrontend
+	clangRewriteCore
+	clangEdit
+	clangAST
+	clangLex
+	clangBasic
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CLANG DEFAULT_MSG CLANG_LIBRARIES CLANG_INCLUDE_DIRS)
+
+mark_as_advanced(CLANG_INCLUDE_DIRS CLANG_LIBRARIES)
