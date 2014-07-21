bf563f92f3e293881ba500040bc5c670841324c2
Eberhard Graether <egraether@gmail.com>
2014-07-21 13:25:18 +0200

    logic: moved font and logger initialization into init() in main

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -6,6 +6,15 @@
 #include "includes.h" // defines 'void setup()'
 #include "qt/QtGuiFactory.h"
 #include "qt/utility/utilityQt.h"
+#include "utility/logging/ConsoleLogger.h"
+#include "utility/logging/LogManager.h"
+
+void init()
+{
+	LogManager::getInstance()->addLogger(std::make_shared<ConsoleLogger>());
+
+	utility::loadFontsFromDirectory("data/fonts", ".otf");
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,10 +23,10 @@ int main(int argc, char *argv[])
 	QApplication qtApp(argc, argv);
 	QtGuiFactory guiFactory;
 
+	init();
+
 	std::shared_ptr<Application> app = Application::create(&guiFactory);
 	app->loadProject("data/ProjectSettings.xml");
-
-	utility::loadFontsFromDirectory("data/fonts", ".otf");
 
 	return qtApp.exec();
 }
--- src/lib/Application.cpp
+++ src/lib/Application.cpp
@@ -5,16 +5,11 @@
 #include "data/access/GraphAccessProxy.h"
 #include "data/access/LocationAccessProxy.h"
 #include "gui/GuiFactory.h"
-#include "utility/logging/ConsoleLogger.h"
-#include "utility/logging/LogManager.h"
 #include "utility/messaging/MessageQueue.h"
 #include "utility/messaging/type/MessageActivateToken.h"
 
 std::shared_ptr<Application> Application::create(GuiFactory* guiFactory)
 {
-	std::shared_ptr<ConsoleLogger> consoleLogger = std::make_shared<ConsoleLogger>(); // TODO: move to main
-	LogManager::getInstance()->addLogger(consoleLogger);
-
 	ApplicationSettings::getInstance()->load("data/ApplicationSettings.xml");
 
 	std::shared_ptr<Application> ptr(new Application());
