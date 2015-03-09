389386838682526c2aa47d62c7c8c172a1281777
Andreas Stallinger <astallinger.mmt-m2013@fh-salzburg.ac.at>
2015-03-09 12:50:55 +0100

    build: Appname change
    Appname changed to coati + build script for unix

---
+++ script/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Determine path to script
+MY_PATH=`dirname "$0"`
+
+cd $MY_PATH/..
+
+# Build and run target
+
+if [ "$1" = "release" ] || [ "$1" = "r" ]
+then
+	if [ $2 = "test" ]
+	then
+		#echo "release test"
+		ninja -C build/Release Coati_test && cd bin/test && Release/Coati_test
+	else
+		#echo "release app"
+		ninja -C build/Release Coati && cd bin/app && Release/Coati
+	fi
+elif [ "$1" = "debug" ] || [ "$1" = "d" ]
+then
+	echo "$2"
+	if [ "$2" = "test" ]
+	then
+		#echo "debug test"
+		ninja -C build/Debug Coati_test && cd bin/test && Debug/Coati_test
+	else
+		#echo "debug app"
+		ninja -C build/Debug Coati && cd bin/app && Debug/Coati
+	fi
+else
+	echo "no arguments first argument 'release' or 'debug', if test second argument is 'test'"
+fi
--- script/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Determine path to script
-MY_PATH=`dirname "$0"`
-
-cd $MY_PATH/..
-
-# Build and run target
-ninja -C build/Release masterproject_"$1" && cd bin/"$1" && Release/masterproject_"$1"
