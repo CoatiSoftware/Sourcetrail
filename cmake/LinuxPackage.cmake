bfaceceb29cb63c33d7008183fb57054edb27da6
Andreas Stallinger <astallinger.mmt-m2013@fh-salzburg.ac.at>
2015-11-27 15:05:14 +0100

    build: remove Coati folder from linux package
    removed the coati folder in Coati-<version> folder, was one folder too much

--- cmake/LinuxPackage.cmake
+++ cmake/LinuxPackage.cmake
@@ -3,67 +3,91 @@
 #     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 # )
 
+# INSTALL(CODE "
+#         execute_process(
+#         COMMAND upx --best ${CMAKE_SOURCE_DIR}/bin/app/Release/Coati)"
+# )
+
 INSTALL(DIRECTORY ${CMAKE_SOURCE_DIR}/bin/app/data
-        DESTINATION coati/data
-        PATTERN "log/*" EXCLUDE
-        PATTERN "projects/ingored" EXCLUDE
-        PATTERN "src" EXCLUDE
-        PATTERN "ApplicationSettings.xml" EXCLUDE
-        PATTERN "ApplicationSettings_for_package.xml" EXCLUDE
-        PATTERN "ProjectSettings_template.xml" EXCLUDE
-        PATTERN "ApplicationSettings_template.xml" EXCLUDE
+    DESTINATION .
+    PATTERN "log/*" EXCLUDE
+    PATTERN "projects/ignored" EXCLUDE
+    PATTERN "data/src" EXCLUDE
+    PATTERN "*.sqlite" EXCLUDE
+    PATTERN "ApplicationSettings.xml" EXCLUDE
+    PATTERN "ApplicationSettings_for_package.xml" EXCLUDE
+    PATTERN "ProjectSettings_template.xml" EXCLUDE
+    PATTERN "ApplicationSettings_template.xml" EXCLUDE
 )
+
+INSTALL(DIRECTORY ${CMAKE_SOURCE_DIR}/ide_plugins/vs
+    DESTINATION plugin
+    PATTERN "vs2012" EXCLUDE
+)
+
+INSTALL(DIRECTORY ${CMAKE_SOURCE_DIR}/ide_plugins/sublime_text
+    DESTINATION plugin
+)
+
 
 INSTALL(FILES ${CMAKE_SOURCE_DIR}/bin/app/data/ApplicationSettings_for_package.xml
-        DESTINATION coati/data
-        RENAME ApplicationSettings.xml)
+    DESTINATION data
+    RENAME ApplicationSettings.xml
+)
 
 INSTALL(FILES ${CMAKE_SOURCE_DIR}/setup/Linux/coati.desktop
-        DESTINATION applications
+    DESTINATION .
 )
+
 INSTALL(FILES ${CMAKE_SOURCE_DIR}/bin/app/data/gui/icon/logo_1024_1024.png
-        DESTINATION icons
-        RENAME coati.png)
+    DESTINATION .
+    RENAME coati.png
+)
 
 INSTALL(FILES ${CMAKE_SOURCE_DIR}/web/coati_manual.pdf
-        DESTINATION coati)
+    DESTINATION .
+)
 
 INSTALL(DIRECTORY ${CMAKE_SOURCE_DIR}/setup/Linux/lib
-        DESTINATION coati
+    DESTINATION .
 )
+
 INSTALL(FILES
-        ${CMAKE_SOURCE_DIR}/setup/Linux/libqxcb.so
-        DESTINATION coati/platforms
+    ${CMAKE_SOURCE_DIR}/setup/Linux/libqxcb.so
+    DESTINATION platforms
 )
 INSTALL(PROGRAMS
-        ${CMAKE_SOURCE_DIR}/setup/Linux/Coati.sh
-        DESTINATION coati
+    ${CMAKE_SOURCE_DIR}/setup/Linux/Coati.sh
+    DESTINATION .
 )
-INSTALL(PROGRAMS ${CMAKE_SOURCE_DIR}/setup/Linux/coati
-        DESTINATION bin/
-)
+# INSTALL(PROGRAMS ${CMAKE_SOURCE_DIR}/setup/Linux/coati
+#         DESTINATION bin/
+# )
 # add the automatically determined parts of the RPATH
 # which point to directories outside the build tree to the install RPATH
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
 INSTALL(TARGETS ${APP_PROJECT_NAME}
-        DESTINATION coati
+    DESTINATION .
 )
 
 # SET(CPACK_GENERATOR "DEB;TGZ")
 SET(CPACK_GENERATOR "TGZ")
 SET(CPACK_ARCHIVE_COMPONENT_INSTALL "ON")
 
-# DEBIAN PACKAGE
-# SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "Andreas Stallinger <astallinger@coati.io>")
-# SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE  "amd64")
-# SET(CPACK_DEBIAN_PACKAGE_HOMEPAGE "http://coati.io")
-# SET(CPACK_DEBIAN_PACKAGE_SECTION "devel")
+string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" VERSION_MAJOR "${GIT_VERSION_NUMBER}")
+string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${GIT_VERSION_NUMBER}")
+string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${GIT_VERSION_NUMBER}")
+set(VERSION_NUMBER "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
 
 SET(CPACK_PACKAGE_NAME "Coati")
 SET(CPACK_PACKAGING_INSTALL_PREFIX "")
-SET(CPACK_PACKAGE_VERSION ${GIT_VERSION_NUMBER})
+SET(CPACK_PACKAGE_VERSION ${VERSION_NUMBER})
+SET(CPACK_PACKAGE_VENDOR "Coati Software")
+# SET(CPACK_PACKAGE_VERSION ${GIT_VERSION_NUMBER})
 SET(CPACK_PACKAGE_EXECUTABLES "Coati")
 SET(CPACK_STRIP_FILES "coati/Coati")
+SET(CPACK_PACKAGE_CONTACT "astallinger@coati.io")
+MESSAGE(STATUS "${CMAKE_SYSTEM}")
 
 INCLUDE(CPack)
