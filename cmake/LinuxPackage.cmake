dd49fcd7092960a8a8c7849ca39c53176aa6526e
Andreas Stallinger <astallinger@coati.io>
2016-09-06 13:05:20 +0200

    build: Linux package base folder
    Added a Coati folder as base to the archive

--- cmake/LinuxPackage.cmake
+++ cmake/LinuxPackage.cmake
@@ -1,23 +1,9 @@
-# execute_process(
-#     COMMAND ${CMAKE_SOURCE_DIR}/setup/Linux/findAndCopyLibraries.sh
-#     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-# )
-
-#INSTALL(CODE
-	#"execute_process(COMMAND \"${APP_PROJECT_NAME}
-	#-d ${CMAKE_SOURCE_DIR}/bin/app/data/projects/tictactoe/tictactoe.coatiproject\")"
-#)
-
-#INSTALL(CODE
-	#"execute_process(COMMAND ${APP_PROJECT_NAME}
-	#-d ${CMAKE_SOURCE_DIR}/bin/app/data/projects/tutorial/tutorial.coatiproject)"
-#)
 
 function(InstallQtModule module qtversion componentName)
 	get_filename_component(realpath ${EXTERNAL_BUILD}/qt/lib/libQt5${module}.so.${qtversion} REALPATH)
 	INSTALL(FILES
 		${realpath}
-		DESTINATION lib
+		DESTINATION Coati/lib
 		COMPONENT ${componentName}
 		RENAME libQt5${module}.so.${qtversion}
 	)
@@ -35,9 +21,10 @@ endfunction(InstallQt)
 function(AddSharedToComponent componentName)
 	INSTALL(DIRECTORY
 		${CMAKE_SOURCE_DIR}/bin/app/data
-		DESTINATION .
+		DESTINATION Coati
 		COMPONENT ${componentName}
 		PATTERN "log/*" EXCLUDE
+		PATTERN "data/projects"
 		PATTERN "data/src" EXCLUDE
 		PATTERN "data/gui/installer" EXCLUDE
 		PATTERN "data/install" EXCLUDE
@@ -48,7 +35,7 @@ function(AddSharedToComponent componentName)
 	)
 
 	INSTALL(FILES ${CMAKE_SOURCE_DIR}/bin/app/user/ApplicationSettings_for_package.xml
-		DESTINATION user
+		DESTINATION Coati/user
 		COMPONENT ${componentName}
 		RENAME ApplicationSettings.xml
 	)
@@ -56,7 +43,7 @@ function(AddSharedToComponent componentName)
 	INSTALL(FILES
 		${CMAKE_SOURCE_DIR}/bin/app/data/gui/installer/EULA.txt
 		COMPONENT ${componentName}
-		DESTINATION .
+		DESTINATION Coati
 	)
 
 	InstallQt(5 ${componentName})
@@ -65,18 +52,18 @@ function(AddSharedToComponent componentName)
 	INSTALL(FILES
 		#C++
 		${realpath}
-		DESTINATION lib
+		DESTINATION Coati/lib
 		COMPONENT ${componentName}
 	)
 
 	INSTALL(FILES
 		${EXTERNAL_BUILD}/qt/plugins/platforms/libqxcb.so
-		DESTINATION platforms
+		DESTINATION Coati/platforms
 		COMPONENT ${componentName}
 	)
 
 	INSTALL(DIRECTORY ${CMAKE_SOURCE_DIR}/bin/app/user
-		DESTINATION .
+		DESTINATION Coati
 		COMPONENT ${componentName}
 		PATTERN "ApplicationSettings.xml" EXCLUDE
 		PATTERN "ApplicationSettings_for_package.xml" EXCLUDE
@@ -86,7 +73,7 @@ function(AddSharedToComponent componentName)
 	)
 
 	INSTALL(FILES ${CMAKE_SOURCE_DIR}/bin/app/user/window_settings_for_package.ini
-		DESTINATION user
+		DESTINATION Coati/user
 		COMPONENT ${componentName}
 		RENAME window_settings.ini
 	)
@@ -101,67 +88,74 @@ execute_process(
 AddSharedToComponent(FULL)
 AddSharedToComponent(TRIAL)
 
-INSTALL(DIRECTORY
-	${CMAKE_SOURCE_DIR}/ide_plugins/sublime_text
-	DESTINATION plugin
-	COMPONENT FULL
-)
+#INSTALL(DIRECTORY
+	#${CMAKE_SOURCE_DIR}/ide_plugins/sublime_text
+	#DESTINATION plugin
+	#COMPONENT FULL
+#)
 
 INSTALL(DIRECTORY
-	${CMAKE_SOURCE_DIR}/ide_plugins/vim
-	DESTINATION plugin
+	${CMAKE_SOURCE_DIR}/ide_plugins/
+	DESTINATION Coati/plugin
 	COMPONENT FULL
+	PATTERN "vs" EXCLUDE
 )
 
 INSTALL(DIRECTORY
 		${CMAKE_SOURCE_DIR}/bin/app/data/projects
-		DESTINATION user
+		DESTINATION Coati/user
 		COMPONENT FULL
 		PATTERN "*.coatidb" EXCLUDE
 )
 
+INSTALL(FILES
+		${CMAKE_SOURCE_DIR}/bin/app/data/projects/javaparser/javaparser.coatidb
+		DESTINATION Coati/user/projects/javaparser
+		COMPONENT FULL
+)
+
 INSTALL(DIRECTORY
 		${CMAKE_SOURCE_DIR}/bin/app/data/projects
-		DESTINATION user
+		DESTINATION Coati/user
 		COMPONENT TRIAL
 )
 
 INSTALL(FILES
 	${CMAKE_SOURCE_DIR}/setup/Linux/coati.desktop
 	${CMAKE_SOURCE_DIR}/setup/Linux/coati-mime.xml
-	DESTINATION setup
+	DESTINATION Coati/setup
 	COMPONENT FULL
 	)
 INSTALL(PROGRAMS
 	${CMAKE_SOURCE_DIR}/setup/Linux/install.sh
 	${CMAKE_SOURCE_DIR}/setup/Linux/deinstall.sh
 	${CMAKE_SOURCE_DIR}/setup/Linux/removeConfigs.sh
-	DESTINATION setup
+	DESTINATION Coati/setup
 	COMPONENT FULL
 	)
 
 INSTALL(PROGRAMS
 	${CMAKE_SOURCE_DIR}/setup/Linux/Coati_trial.sh
-	DESTINATION .
+	DESTINATION Coati
 	COMPONENT TRIAL
 	)
 
 INSTALL(PROGRAMS
 	${CMAKE_SOURCE_DIR}/setup/Linux/Coati.sh
-	DESTINATION .
+	DESTINATION Coati
 	COMPONENT FULL
 )
 
 INSTALL(PROGRAMS
 	${CMAKE_SOURCE_DIR}/bin/app/Release/Coati_upx
-	DESTINATION .
+	DESTINATION Coati
 	COMPONENT FULL
 	RENAME Coati
 )
 
 INSTALL(PROGRAMS
 	${CMAKE_SOURCE_DIR}/bin/app/Release/Coati_trial_upx
-	DESTINATION .
+	DESTINATION Coati
 	COMPONENT TRIAL
 	RENAME Coati_trial
 )
@@ -184,28 +178,29 @@ SET(CPACK_ARCHIVE_COMPONENT_INSTALL "ON")
 
 string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" VERSION_MAJOR "${GIT_VERSION_NUMBER}")
 string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${GIT_VERSION_NUMBER}")
-string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${GIT_VERSION_NUMBER}")
-if(${GIT_VERSION_NUMBER} STREQUAL ${VERSION_PATCH})
-	SET(VERSIONA_PATCH 0)
-endif()
-string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.[0-9]+-([0-9]+).*" "\\1" VERSION_COMMIT "${GIT_VERSION_NUMBER}")
+#string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${GIT_VERSION_NUMBER}")
+#if(${GIT_VERSION_NUMBER} STREQUAL ${VERSION_PATCH})
+	#SET(VERSIONA_PATCH 0)
+#endif()
+string(REGEX REPLACE "^[0-9]+\\.[0-9]+-([0-9]+).*" "\\1" VERSION_COMMIT "${GIT_VERSION_NUMBER}")
+
 if(${GIT_VERSION_NUMBER} STREQUAL ${VERSION_COMMIT})
-	set(VERSION_NUMBER "${VERSION_MAJOR}_${VERSION_MINOR}_${VERSION_PATCH}")
+	set(VERSION_NUMBER "${VERSION_MAJOR}_${VERSION_MINOR}")
 else()
-	set(VERSION_NUMBER "${VERSION_MAJOR}_${VERSION_MINOR}_${VERSION_PATCH}_${VERSION_COMMIT}")
+	set(VERSION_NUMBER "${VERSION_MAJOR}_${VERSION_MINOR}_${VERSION_COMMIT}")
 endif()
 
 SET(CPACK_PACKAGE_NAME "Coati")
 SET(CPACK_PACKAGING_INSTALL_PREFIX "")
 SET(CPACK_INCLUDE_TOPLEVEL_DIRECTORY 1)
+#SET(CPACK_TOPLEVEL_TAG "Coati")
 SET(CPACK_PACKAGING_INSTALL_DIRECTORY "Coati")
 SET(CPACK_PACKAGE_VERSION ${VERSION_NUMBER})
 SET(CPACK_PACKAGE_VENDOR "Coati Software")
 SET(CPACK_INSTALL_SCRIPT "${CMAKE_SOURCE_DIR}/cmake/pre_install_linux.cmake")
 SET(CPACK_SYSTEM_NAME "${CMAKE_SYSTEM_NAME}_${CMAKE_SYSTEM_PROCESSOR}")
 SET(CPACK_PACKAGE_FILE_NAME "Coati_${VERSION_NUMBER}_${CPACK_SYSTEM_NAME}")
-#SET(CPACK_PACKAGE_EXECUTABLES "Coati")
-SET(CPACK_STRIP_FILES "coati/Coati")
+SET(CPACK_STRIP_FILES "Coati/coati")
 SET(CPACK_PACKAGE_CONTACT "astallinger@coati.io")
 
 INCLUDE(CPack)
--- cmake/pre_install_linux.cmake
+++ cmake/pre_install_linux.cmake
@@ -2,18 +2,6 @@
 # upx
 set(upxPath ${CMAKE_CURRENT_LIST_DIR}/../bin/app/Release)
 
-message(STATUS "upx the app")
-execute_process(
-	COMMAND rm ${upxPath}/Coati_upx
-	COMMAND upx --brute ${upxPath}/Coati -o ${upxPath}/Coati_upx
-)
-
-message(STATUS "upx the trial")
-execute_process(
-	COMMAND rm ${upxPath}/Coati_trial_upx
-	COMMAND upx --brute ${upxPath}/Coati_trial -o ${upxPath}/Coati_trial_upx
-)
-
 execute_process(
 	COMMAND ${upxPath}/Coati -z /home/vagrant/dev/license.txt
 )
@@ -26,6 +14,35 @@ execute_process(
 	COMMAND ${upxPath}/Coati -f -d ${upxPath}/../data/projects/tutorial/tutorial.coatiproject
 )
 
+#execute_process(
+	#COMMAND ${upxPath}/Coati -f -d ${upxPath}/../data/projects/javaparser/javaparser.coatiproject
+#)
+
+set(bla 0)
+
+if (bla)
+
+	message(STATUS "upx the app")
+	execute_process(
+		COMMAND rm ${upxPath}/Coati_upx
+		COMMAND upx --brute ${upxPath}/Coati -o ${upxPath}/Coati_upx
+	)
+
+	message(STATUS "upx the trial")
+	execute_process(
+		COMMAND rm ${upxPath}/Coati_trial_upx
+		COMMAND upx --brute ${upxPath}/Coati_trial -o ${upxPath}/Coati_trial_upx
+	)
+
+else()
+	execute_process(
+		COMMAND rm ${upxPath}/Coati_trial_upx
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different ${upxPath}/Coati_trial ${upxPath}/Coati_trial_upx
+		COMMAND rm ${upxPath}/Coati_upx
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different ${upxPath}/Coati ${upxPath}/Coati_upx
+	)
+
+endif()
 #add_custom_command(
 	#OUTPUT Coati_upx
 	#COMMAND upx --brute ${CMAKE_CURRENT_LIST_DIR}/../bin/app/Release/Coati -o ${CMAKE_CURRENT_LIST_DIR}/../bin/app/Release/Coati_upx
