06676eda662dcce3c32ae87268b01d86db7b1b3f
Eberhard Graether <egraether@gmail.com>
2017-02-09 13:37:40 +0100

    logic: Fixed comparing of project files to ignore carriage return characters

--- src/lib/Project.cpp
+++ src/lib/Project.cpp
@@ -209,7 +209,8 @@ void Project::load()
 	{
 		m_state = PROJECT_STATE_OUTVERSIONED;
 	}
-	else if (TextAccess::createFromFile(projectSettingsPath.str())->getText() != m_storage->getProjectSettingsText())
+	else if (utility::replace(TextAccess::createFromFile(projectSettingsPath.str())->getText(), "\r", "") !=
+		utility::replace(TextAccess::createFromString(m_storage->getProjectSettingsText())->getText(), "\r", ""))
 	{
 		m_state = PROJECT_STATE_OUTDATED;
 		canLoad = true;
