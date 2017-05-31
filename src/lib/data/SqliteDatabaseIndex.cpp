8ab0abbe69ba978588d6dc45f74039aee730df15
Eberhard Graether <egraether@gmail.com>
2017-05-31 14:07:21 +0200

    data: Added exception handling to database index

--- src/lib/data/SqliteDatabaseIndex.cpp
+++ src/lib/data/SqliteDatabaseIndex.cpp
@@ -1,5 +1,7 @@
 #include "data/SqliteDatabaseIndex.h"
 
+#include "utility/logging/logging.h"
+
 SqliteDatabaseIndex::SqliteDatabaseIndex(const std::string& indexName, const std::string& indexTarget)
 	: m_indexName(indexName)
 	, m_indexTarget(indexTarget)
@@ -12,14 +14,30 @@ SqliteDatabaseIndex::~SqliteDatabaseIndex()
 
 void SqliteDatabaseIndex::createOnDatabase(CppSQLite3DB& database)
 {
-	database.execDML((
-		"CREATE INDEX IF NOT EXISTS " + m_indexName + " ON " + m_indexTarget + ";"
-	).c_str());
+	try
+	{
+		LOG_INFO_STREAM(<< "Creating database index \"" << m_indexName << "\"");
+		database.execDML((
+			"CREATE INDEX IF NOT EXISTS " + m_indexName + " ON " + m_indexTarget + ";"
+		).c_str());
+	}
+	catch (CppSQLite3Exception e)
+	{
+		LOG_ERROR(std::to_string(e.errorCode()) + ": " + e.errorMessage());
+	}
 }
 
 void SqliteDatabaseIndex::removeFromDatabase(CppSQLite3DB& database)
 {
-	database.execDML((
-		"DROP INDEX IF EXISTS main." + m_indexName + ";"
-	).c_str());
+	try
+	{
+		LOG_INFO_STREAM(<< "Removing database index \"" << m_indexName << "\"");
+		database.execDML((
+			"DROP INDEX IF EXISTS main." + m_indexName + ";"
+		).c_str());
+	}
+	catch (CppSQLite3Exception e)
+	{
+		LOG_ERROR(std::to_string(e.errorCode()) + ": " + e.errorMessage());
+	}
 }
