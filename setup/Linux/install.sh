8b6109be79ce20b940faaab530fbbbaced66d02b
Andreas Stallinger <astallinger@coati.io>
2017-06-20 15:36:43 +0200

    script: install script linux fix
    install script failed when there were spaces in the path

--- setup/Linux/install.sh
+++ setup/Linux/install.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 MY_PATH=$SCRIPT_DIR/..
 
-cat ${MY_PATH}/EULA.txt
+cat "${MY_PATH}/EULA.txt"
 echo "Agree to the EULA"
 printf 'enter [y/n] '
 read ans
@@ -9,10 +9,10 @@ case ${ans:=y} in [yY]*) ;; *) exit ;; esac
 
 echo "Run this script as root"
 
-cp -rf $MY_PATH/ /opt/sourcetrail/ > /dev/null
-cp -rf $MY_PATH/setup/share/ /usr > /dev/null
-cp $MY_PATH/data/gui/icon/logo_1024_1024.png /usr/share/icons/sourcetrail.png > /dev/null
-cp $MY_PATH/data/gui/icon/project_256_256.png /usr/share/icons/project-sourcetrail.png > /dev/null
+cp -rf "$MY_PATH/" /opt/sourcetrail/ > /dev/null
+cp -rf "$MY_PATH/setup/share/" /usr > /dev/null
+cp "$MY_PATH/data/gui/icon/logo_1024_1024.png" /usr/share/icons/sourcetrail.png > /dev/null
+cp "$MY_PATH/data/gui/icon/project_256_256.png" /usr/share/icons/project-sourcetrail.png > /dev/null
 update-mime-database /usr/share/mime > /dev/null
 update-desktop-database > /dev/null
 
