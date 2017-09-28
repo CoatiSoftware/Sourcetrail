a11870ad777cf595aabd59d39d3de1e0a9c89918
mlangkabel <malte.langkabel@gmail.com>
2017-09-28 18:03:16 +0200

    src: removed unused methods from Java indexer code

--- java_indexer/src/main/java/com/sourcetrail/AstVisitorClient.java
+++ java_indexer/src/main/java/com/sourcetrail/AstVisitorClient.java
@@ -1,66 +1,9 @@
 package com.sourcetrail;
 
-import java.util.Optional;
-
 import com.sourcetrail.name.NameHierarchy;
 
 public abstract class AstVisitorClient 
 {
-	public void recordSymbolWithLocation(
-			NameHierarchy symbolName, SymbolKind symbolKind, 
-			Optional<Range> range,
-			AccessKind access, DefinitionKind definitionKind)
-	{
-		recordSymbolWithLocation(
-				symbolName, symbolKind, 
-				range.orElse(new Range(0, 0, 0, 0)),
-				access, definitionKind);
-	}
-	
-	public void recordSymbolWithLocationAndScope(
-			NameHierarchy symbolName, SymbolKind symbolKind, 
-			Optional<Range> range,
-			Optional<Range> scopeRange,
-			AccessKind access, DefinitionKind definitionKind)
-	{
-		recordSymbolWithLocationAndScope(
-			symbolName, symbolKind, 
-			range.orElse(new Range(0, 0, 0, 0)),
-			scopeRange.orElse(new Range(0, 0, 0, 0)),
-			access, definitionKind);
-	}
-	
-	public void recordReference(
-			ReferenceKind referenceKind, NameHierarchy referencedName, NameHierarchy contextName, 
-			Optional<Range> range)
-	{
-		recordReference(
-				referenceKind, referencedName, contextName, 
-				range.orElse(new Range(0, 0, 0, 0)));
-	}
-	
-	public void recordLocalSymbol(
-			NameHierarchy symbolName,
-			Optional<Range> range)
-	{
-		recordLocalSymbol(symbolName, range.orElse(new Range(0, 0, 0, 0)));
-	}
-	
-	public void recordComment(
-			Optional<Range> range)
-	{
-		recordComment(range.orElse(new Range(0, 0, 0, 0)));
-	}
-	
-	public void recordError(
-		String message, boolean fatal, boolean indexed, 
-		Optional<Range> range)
-	{
-		recordError(
-				message, fatal, indexed, 
-				range.orElse(new Range(0, 0, 0, 0)));
-	}
-	
 	public abstract boolean getInterrupted();
 	
 	public abstract void logInfo(String info);
