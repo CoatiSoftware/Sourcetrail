66b278b1c98efa46c5407eb709208deae24a53a0
Eberhard Graether <egraether@gmail.com>
2017-11-18 00:24:01 +0100

    script: Fixed Sourcetrail.sh syntax error (issue #519)

--- setup/Linux/Sourcetrail.sh
+++ setup/Linux/Sourcetrail.sh
@@ -5,7 +5,9 @@ SOURCE="${0}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
 	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 	SOURCE="$(readlink "$SOURCE")"
-	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	if [ $SOURCE != /* ]; then
+		SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	fi
 done
 SOURCETRAIL_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
