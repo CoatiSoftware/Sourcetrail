c7cc70c22ee1edc9ce999283aa32ec82870f0d32
Eberhard Graether <egraether@gmail.com>
2018-12-06 18:08:40 +0100

    build: Fixed issue in sourcetrail.sh Linux script (issue #638)

--- setup/Linux/Sourcetrail.sh
+++ setup/Linux/Sourcetrail.sh
@@ -5,9 +5,10 @@ SOURCE="${0}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
 	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 	SOURCE="$(readlink "$SOURCE")"
-	if [ "$SOURCE" != /* ]; then
-		SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-	fi
+	case $SOURCE in # if $SOURCE is a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	  /*) ;;
+	  *) SOURCE="$DIR/$SOURCE" ;;
+	esac
 done
 SOURCETRAIL_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
