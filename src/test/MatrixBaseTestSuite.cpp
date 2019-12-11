7f655964a2ad1ffbfe05f9502b751dc3fe17ed8f
Alexey Andronov <andronovcpp@gmail.com>
2019-12-11 20:02:27 +0700

    Explicitly suppress warnings for tests
    Warning C4244: 'argument' : conversion from 'type1' to 'type2', possible loss of data

--- src/test/MatrixBaseTestSuite.cpp
+++ src/test/MatrixBaseTestSuite.cpp
@@ -197,6 +197,8 @@ TEST_CASE("matrixBase multiplyDivideScalar")
 {
 	MatrixBase<int, 3, 5> matrix0 = getTestMatrix3x5();
 
+#pragma warning(push)
+#pragma warning(disable : 4244)
 	matrix0.scalarMultiplication(2.0f);	   // float is on porpoise (so is porpoise, womp womp)
 
 	REQUIRE(0 == matrix0.getValue(0, 0));
@@ -220,6 +222,7 @@ TEST_CASE("matrixBase multiplyDivideScalar")
 	REQUIRE(0 == matrix0.getValue(0, 0));
 	REQUIRE(0 == matrix0.getValue(1, 1));
 	REQUIRE(1 == matrix0.getValue(2, 2));
+#pragma warning(pop)
 }
 
 TEST_CASE("matrixBase multiplyMatrix")
