86ecb797f401395c4ac2efbd82a1fda418e72604
mlangkabel <malte.langkabel@gmail.com>
2020-01-13 12:57:47 +0100

    add tests for gradle utility functions

---
+++ bin/test/data/UtilityGradleTestSuite/simple_gradle_project/src/main/java/hello/Greeter.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello world!";
+  }
+}
---
+++ src/test/UtilityGradleTestSuite.cpp
@@ -0,0 +1,39 @@
+#include "catch.hpp"
+
+#include "language_packages.h"
+
+#if BUILD_JAVA_LANGUAGE_PACKAGE
+
+#	include "FilePath.h"
+#	include "utility.h"
+#	include "utilityGradle.h"
+
+TEST_CASE("gradle wrapper detects source directories of simple projects")
+{
+	std::vector<FilePath> result = utility::gradleGetAllSourceDirectories(
+		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project"), false);
+
+	REQUIRE(utility::containsElement<FilePath>(
+		result,
+		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/main/java").makeAbsolute()));
+
+	REQUIRE(!utility::containsElement<FilePath>(
+		result,
+		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/test/java").makeAbsolute()));
+}
+
+TEST_CASE("gradle wrapper detects source and test directories of simple projects")
+{
+	std::vector<FilePath> result = utility::gradleGetAllSourceDirectories(
+		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project"), true);
+
+	REQUIRE(utility::containsElement<FilePath>(
+		result,
+		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/main/java").makeAbsolute()));
+
+	REQUIRE(utility::containsElement<FilePath>(
+		result,
+		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/test/java").makeAbsolute()));
+}
+
+#endif	  // BUILD_JAVA_LANGUAGE_PACKAGE
