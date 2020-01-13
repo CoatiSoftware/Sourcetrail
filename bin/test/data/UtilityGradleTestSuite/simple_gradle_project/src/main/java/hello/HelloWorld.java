c8dcb193ff524ecfdd864120f73b5fc0746f63d2
Malte Langkabel <malte.langkabel@gmail.com>
2020-01-13 19:26:17 +0100

    test: added tests for gradle utility functions (#892)

---
+++ bin/test/data/UtilityGradleTestSuite/simple_gradle_project/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -5,6 +5,8 @@
 // because it interferes with the jni interface that emits such a signal on purpose
 
 #include "ApplicationSettings.h"
+#include "language_packages.h"
+#include "utilityPathDetection.h"
 
 struct EventListener: Catch::TestEventListenerBase
 {
@@ -31,6 +33,18 @@ struct EventListener: Catch::TestEventListenerBase
 		}
 #else
 		ApplicationSettings::getInstance()->load(FilePath(L"data/TestSettings.xml"));
+#endif
+
+#if BUILD_JAVA_LANGUAGE_PACKAGE
+		if (ApplicationSettings::getInstance()->getJavaPath().empty())
+		{
+			std::shared_ptr<PathDetector> pathDetector = utility::getJavaRuntimePathDetector();
+			const std::vector<FilePath> paths = pathDetector->getPaths();
+			if (!paths.empty())
+			{
+				ApplicationSettings::getInstance()->setJavaPath(paths.front());
+			}
+		}
 #endif
 	}
 };
