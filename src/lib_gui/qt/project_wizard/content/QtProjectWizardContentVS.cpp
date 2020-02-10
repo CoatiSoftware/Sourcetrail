dc1eaea91a005949947e422a97df2e8ecbc3a115
Eberhard Graether <egraether@gmail.com>
2020-02-10 20:11:59 +0100

    updated content VS texts

--- src/lib_gui/qt/project_wizard/content/QtProjectWizardContentVS.cpp
+++ src/lib_gui/qt/project_wizard/content/QtProjectWizardContentVS.cpp
@@ -14,15 +14,17 @@ void QtProjectWizardContentVS::populate(QGridLayout* layout, int& row)
 
 	addHelpButton(
 		"Create Compilation Database",
-		"To create a new Compilation Database from a Visual Studio Solution, this Solution has to be open in Visual Studio.\n\
-Sourcetrail will call Visual Studio to open the 'Create Compilation Database' dialog.\
- Please follow the instructions in Visual Studio to complete the process.\n\
-Note: Sourcetrail's Visual Studio plugin has to be installed. Visual Studio has to be running with an eligible Solution, containing C/C++ projects, loaded.",
+		"To create a new Compilation Database from a Visual Studio Solution, a Solution has to be open in Visual "
+		"Studio.\n Sourcetrail will call Visual Studio to open the 'Create Compilation Database' dialog. Please follow "
+		"the instructions in Visual Studio to complete the process.\n Note: Sourcetrail's Visual Studio plugin has to "
+		"be installed. Visual Studio has to be running with an eligible Solution, containing C/C++ projects, loaded.",
 		layout,
 		row);
 
 	QLabel* descriptionLabel = createFormSubLabel(
-		"Call Visual Studio to create a Compilation Database from the loaded Solution.");
+		"Call Visual Studio to create a Compilation Database from the loaded Solution (requires installed "
+		"<a href=\"https://sourcetrail.com/documentation/index.html#VisualStudio\">Sourcetrail Visual Studio "
+		"Extension</a>).");
 	descriptionLabel->setObjectName("description");
 	descriptionLabel->setAlignment(Qt::AlignmentFlag::AlignLeft);
 	layout->addWidget(descriptionLabel, row, QtProjectWizardWindow::BACK_COL);
