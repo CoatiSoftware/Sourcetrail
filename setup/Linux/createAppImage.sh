40eb9d856dbe548ef7978d852dfcfd1fbc993682
Eberhard Graether <egraether@gmail.com>
2020-03-29 14:28:36 +0200

    rename AppImage file to current convention

--- setup/Linux/createAppImage.sh
+++ setup/Linux/createAppImage.sh
@@ -64,3 +64,9 @@ export VERSION=$(cat build/Release/version.txt)
 linuxdeployqt AppDir/usr/bin/sourcetrail_indexer -qmake=$Qt5_DIR/bin/qmake -ignore-glob=*python*
 rm -f AppDir/AppRun
 linuxdeployqt AppDir/usr/share/applications/sourcetrail.desktop -qmake=$Qt5_DIR/bin/qmake -ignore-glob=*python* -appimage
+
+rename x86_64 Linux_64bit *.AppImage
+rename - _ *.AppImage
+rename - _ *.AppImage
+rename . _ *.AppImage
+rename . _ *.AppImage
