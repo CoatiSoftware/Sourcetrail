94c03256edcf77611c38d89f238158a346d81e0f
Eberhard Graether <egraether@gmail.com>
2020-06-07 21:20:11 +0200

    compressing

--- setup/Linux/script/createPackage.sh
+++ setup/Linux/script/createPackage.sh
@@ -2,16 +2,21 @@
 
 source createAppDir.sh
 
-export VERSION=$(cat build/Release/version.txt)
+VERSION=$(cat build/Release/version.txt)
+VERSION=${VERSION/./_}
+VERSION=${VERSION/./_}
 
 linuxdeployqt AppDir/usr/bin/sourcetrail_indexer -qmake=$Qt5_DIR/bin/qmake -ignore-glob=*python*
 rm -f AppDir/AppRun
+
 linuxdeployqt AppDir/usr/share/applications/sourcetrail.desktop -qmake=$Qt5_DIR/bin/qmake -ignore-glob=*python*
 rm -f AppDir/AppRun
-cp -R setup/Linux/data/package AppDir/usr
 
-# rename x86_64 Linux_64bit *.AppImage
-# rename - _ *.AppImage
-# rename - _ *.AppImage
-# rename . _ *.AppImage
-# rename . _ *.AppImage
+cp -R setup/Linux/data/package/* AppDir/usr
+
+mv AppDir/usr/ Sourcetrail
+
+tar -czvf Sourcetrail_${VERSION}_Linux_64bit.tar.gz AppDir/Sourcetrail
+
+rm -rf AppDir
+rm -rf Sourcetrail
